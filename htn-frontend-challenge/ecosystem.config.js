e725a47719564f86dbb0631ee8c16b140ff7853a
Zhehai Zhang <zhehaizhang1@gmail.com>
2021-03-16 16:19:27 -0400

    pm2 cofig

---
+++ htn-frontend-challenge/ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'HTN Web App',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start'
+    }
+  ]
+}
